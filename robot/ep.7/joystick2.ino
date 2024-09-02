c67ca40c2da84bef9600bd077819788f5f9fc678
kanyakorn1 <113582008+kanyakorn1@users.noreply.github.com>
2024-09-03 01:47:56 +0700

    Add files via upload

---
+++ robot/ep.7/joystick2.ino
@@ -0,0 +1,49 @@
+#define x_pin A0
+#define y_pin A1
+#define sw_pin 3
+
+#define x_led 10
+#define y_led 9
+#define sw_led 8
+
+void setup(){
+  pinMode(x_pin, INPUT);
+  pinMode(y_pin, INPUT);
+  pinMode(sw_pin, INPUT);
+
+  pinMode(x_led, INPUT);
+  pinMode(y_led, INPUT);
+  pinMode(sw_led, INPUT);
+
+  Serial.begin(9600);
+
+}
+
+void loop(){
+  int x_data = analogRead(x_pin);
+  int y_data = analogRead(y_pin);
+  int sw_data = digitalRead(sw_pin);
+
+  int x_out = map(x_data,0,1023,0,255);
+  int y_out = map(x_data,0,1023,0,255);
+
+  analogWrite(x_led, x_out);
+  analogWrite(y_led, y_out);
+  digitalWrite(sw_led, sw_data);
+
+  Serial.print("ข้อมูลออกที่ LED สีเขียว :");
+  Serial.println(x_out);
+
+  Serial.print("ข้อมูลออกที่ LED สีเหลือง:");
+  Serial.println(y_out);
+
+  Serial.print("ข้อมูลป่มสวิตซ์:");
+  Serial.println(sw_data);
+
+  delay(100);
+
+}
+
+
+
+
